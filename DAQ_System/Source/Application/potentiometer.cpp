1165722515ba3856e95d5a1162224cdd14fc2c52
Manuel De Jesus Contreras <mad210014@utdallas.edu>
2022-10-05 18:06:49 -0500

    Fixed some styling, will continue to fix. Added more implementation in Potentiometer class, can read in mm units based on datasheet, real data could be replaced later

--- DAQ_System/Source/Application/potentiometer.cpp
+++ DAQ_System/Source/Application/potentiometer.cpp
@@ -5,32 +5,35 @@ implements AnalogSensor as a potentiometer with a coefficient.
 */
 #include "mbed.h"
 #include "AnalogIn.h"
-#include "analogSensor.tpp"
+#include "analog_sensor.tpp"
 
 class Potentiometer : public AnalogSensor {
     public:
-        Potentiometer(PinName p, double vCoeff) : AnalogSensor(p) {
-            vC = vCoeff;
+        Potentiometer(PinName p, string name, string units, double vCoeff) : AnalogSensor(p, name, units) {
+            vC_ = vCoeff;
         }
 
-        Potentiometer(PinName p) : AnalogSensor(p) {
-            vC = 1.0;
+        Potentiometer(PinName p, string name, string units) : AnalogSensor(p, name, units) {
+            vC_ = 1.0;
         }
 
-        //sls1300 pot goes from 1-14k ohm
-        ///reads potentiometer and converts to cm
+        //sls1300 pot goes from 1-14k ohm, v = 3.3v, vdiv formula. Should return values from 100 to 450
+        
+        ///reads potentiometer and converts to mm
         double read(){
-            return AnalogIn(pin) * vC;
+            //      Kohm = (adcin * (V/steps) / Itot)
+
+            return 25*(AnalogIn(pin_)*(3.3/4096)/.2357143) * vC_ + 100;
         }
 
         void changePin(PinName p){
-            pin = p;
+            pin_ = p;
         }
         
         double getVCoeff(){
-            return vC;
+            return vC_;
         }
     private:
-        double vC;
-        PinName pin;
+        double vC_;
+        PinName pin_;
 };
