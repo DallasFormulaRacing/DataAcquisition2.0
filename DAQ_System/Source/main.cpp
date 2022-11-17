8c01131806f1fb9505779a48434318db7bb81eaa
TealT34L <djcmanuel77@gmail.com>
2022-11-16 20:53:47 -0600

    starter code for basic CAN implementation and testing

--- DAQ_System/Source/main.cpp
+++ DAQ_System/Source/main.cpp
@@ -1,10 +1,21 @@
 #include "mbed.h"
+#include "iostream"
 
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
+    CAN can(PD_1, PD_0, 250000);
 
+    while (true) {
+        CANMessage msg;
+
+        can.read(msg);
+        cout << msg.id << " ";
+
+        for (char x : msg.data){
+            cout << x << " ";
+        }
+        cout << endl;
     }
 }
 
