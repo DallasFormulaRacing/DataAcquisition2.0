3eb8eabb49a802af5237bef3b598f7d1bf24c264
Demo User <debian@beaglebone.localdomain>
2022-12-24 02:30:50 +0000

    BeagleBone Black gtkmm code

---
+++ DAQ_Dash/base.cpp
@@ -0,0 +1,11 @@
+#include <gtkmm.h>
+
+int main(int argc, char* argv[])
+{
+  auto app = Gtk::Application::create("org.gtkmm.examples.base");
+
+  Gtk::Window window;
+  window.set_default_size(200, 200);
+
+  return app->run(window);
+}
