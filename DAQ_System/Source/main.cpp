d259f16b04f7e5cad709e4aa365cc053d1455d7d
Cristian Cruz <cris14.alex@gmail.com>
2023-01-30 00:51:05 -0600

    Added reference link

--- DAQ_System/Source/main.cpp
+++ DAQ_System/Source/main.cpp
@@ -1,21 +1,23 @@
 #include "mbed.h"
 #include <iostream>
+
 #include "L3GD20H.h"
+// Found in: https://os.mbed.com/users/julioefajardo/code/L3GD20H/docs/tip/classL3GD20H.html
+
 
 // main() runs in its own thread in the OS
 int main()
 {
-    L3GD20H Gyro(PB_7, PB_6);
+    L3GD20H Gyro(I2C_SDA, I2C_SCL);
 
     short gyroData[3] = {0, 0, 0};
 
     while (true) {
-        Gyro.read(&gyroData);
+        Gyro.read(gyroData);
         std::cout << "It's Gyroin Time"
-                  << "\nX: " << gyroData[0]
-                  << "\nY: " << gyroData[1]
-                  << "\nZ: " << gyroData[2] << std::endl;
-        ThisThread::sleep_for(500ms);      // Remove before submission
+                  << "\tX: " << gyroData[0]
+                  << "\tY: " << gyroData[1]
+                  << "\tZ: " << gyroData[2] << std::endl;
+        ThisThread::sleep_for(500ms);
     }
 }
-
