083bfac38c9d7fedd4630271b45035010a1a8e5e
crsz20 <cris14.alex@gmail.com>
2023-04-09 01:02:38 -0500

    renamed sampling variables to logging for transparency

--- DAQ_System/Source/main.cpp
+++ DAQ_System/Source/main.cpp
@@ -16,8 +16,12 @@
 #include "Adapter/Interfaces/ilinear_potentiometer.hpp"
 #include "platform/component_interface_bridge.hpp"
 
-// This file should not use hardware-dependent includes
-// TODO: A component that serves component assembly using abstract classes
+using AutoReloadTimer = LowPowerTicker;
+
+static bool logging = false;
+static void start_logging() {
+    logging = true;
+}
 
 int main() {
     // Init
@@ -25,13 +29,23 @@ int main() {
 
     std::unique_ptr<adapter::ILinear_Potentiometer> linear_potentiometer = bridge.GetLinearPotentiometer();
 
+    
+    constexpr uint8_t kLoggingRate = 3;
+    AutoReloadTimer timer;
+    timer.attach(&start_logging, std::chrono::seconds(kLoggingRate));
 
+    float timestamp = 0.0f;
 
     // Operate
     while (true) {
         linear_potentiometer->ComputeDisplacementPercentage();
-        double displacement_percentage = linear_potentiometer->GetDisplacementPercentage();
-        std::cout << displacement_percentage << "%" << std::endl;
-    }
 
+        if (logging) {
+            // Operate: Writing
+            timestamp += kLoggingRate;
+            std::cout << linear_potentiometer->GetDisplacementPercentage() << "%" << std::endl;
+            
+            logging = false;
+        }
+    }
 }
