8a6a8ca284a629517d82f1ee6f63b414a7859b5f
William Lim <william.lim781@gmail.com>
2024-02-05 11:37:26 -0600

    added the .cpp and .hpp files of the LSM6DSOX

---
+++ Project/DAQ_System/Program/Src/Sensor/Accelerometer/LSM6DSOXAccelerometer.cpp
@@ -0,0 +1,130 @@
+/*
+* Accelerometer LSM6DSOX
+* Authors:       William Lim
+*
+* Email:         wll180000@utdallas.edu
+*
+* (c) 2023 Dallas Formula Racing - Embedded Firmware Team
+* Formula SAE International Collegiate Chapter
+* GPL-3.0 License
+*/
+
+#include "LSM6DSOXAccelerometer.hpp"
+#include <stdint.h>
+#include <cmath>
+#include <limits>
+
+
+namespace sensor{
+
+LSM6DSOX::LSM6DSOX(I2C_HandleTypeDef& hi2c):i2c_(hi2c){}
+
+void LSM6DSOX::init() {
+    static constexpr uint8_t kNumBytes = 2;
+    uint8_t commands[kNumBytes] = {0};
+
+    // data rate 104hz
+    commands[0] = CTRL1_XL;
+    commands[1] = 0x40; // 0b01000000
+    HAL_I2C_Master_Transmit(&i2c_,ACC_ADDRESS, commands,kNumBytes, HAL_MAX_DELAY);
+
+    calibrate();
+}
+
+void LSM6DSOX::calibrate() {
+    static constexpr uint8_t kSampleCount = 100;
+    double acc_magnitude = 0;
+    double average_array[kSampleCount];
+
+    // Collects samples to calibrate for 1G
+    for(int i = 0; i < kSampleCount; i++) {
+        ReadRawAcceleration();
+        acc_magnitude = sqrt(raw_acceleration_data_[0] * raw_acceleration_data_[0] + raw_acceleration_data_[1] * raw_acceleration_data_[1] + raw_acceleration_data_[2] * raw_acceleration_data_[2]);
+        average_array[i] = acc_magnitude * 0.001;
+    }
+
+    double min_value = std::numeric_limits<double>::max();
+    double max_value = -1;
+
+    // Find the largest and smallest magnitudes
+    for(int j = 2; j < kSampleCount; j++) {
+        if(average_array[j] <= min_value) {
+            min_value = average_array[j];
+
+        } else if (average_array[j] > max_value) {
+            max_value = average_array[j];
+        }
+    }
+
+    // Check that the maximum deviation is below a certain threshold.
+    // If it is too large, the conversion factor will be set to a previously
+    // determined conversion factor
+    gravity_adjustment_conversion_factor_ = 0;
+
+    if (max_value - min_value < 0.09) {
+        double total_array = 0;
+	    for(int i = 0; i < kSampleCount; i++) {
+		    total_array += average_array[i];
+	    }
+
+	    gravity_adjustment_conversion_factor_ = total_array / kSampleCount;
+    } else {
+        // Testing found that ~1.028 is about 1G
+        gravity_adjustment_conversion_factor_ = 1.028;
+    }
+}
+
+void LSM6DSOX::SetScale(float x, float y, float z) {
+    scale_[0] = x;
+    scale_[1] = y;
+    scale_[2] = z;
+}
+
+void LSM6DSOX::SetOffset(float x, float y, float z) {
+    offset_[0] = x;
+    offset_[1] = y;
+    offset_[2] = z;
+}
+
+float* LSM6DSOX::GetAcceleration() {
+    return real_acceleration_data_;
+}
+
+void LSM6DSOX::ComputeAcceleration() {
+    ReadRawAcceleration();
+/*   old calculations for accelerometer
+    static constexpr float kMG_LSB = .001F;
+    real_acceleration_data_[0] = (raw_acceleration_data_[0]) * kMG_LSB / gravity_adjustment_conversion_factor_;
+    real_acceleration_data_[1] = (raw_acceleration_data_[1]) * kMG_LSB / gravity_adjustment_conversion_factor_;
+    real_acceleration_data_[2] = (raw_acceleration_data_[2]) * kMG_LSB / gravity_adjustment_conversion_factor_;
+*/
+
+    // linearization of the accelerometer. by default range of the accelerometer is ±2 g
+    real_acceleration_data_[0] = 0.0006 *raw_acceleration_data_[0] - 0.0002;
+    real_acceleration_data_[1] = 0.0006 *raw_acceleration_data_[1] - 0.0002;
+    real_acceleration_data_[2] = 0.0006 *raw_acceleration_data_[2] - 0.0002;
+}
+
+void LSM6DSOX::ReadRawAcceleration() {
+	static constexpr int ByteArraySize = 6;
+	static constexpr int I2CWriteSize = 1;
+	static constexpr int I2CReadSize = 6;
+
+    uint8_t command[1] = { CTRL1_XL };
+    HAL_I2C_Master_Transmit(&i2c_,ACC_ADDRESS, command,I2CWriteSize, HAL_MAX_DELAY);
+
+    uint8_t bytes_received[ByteArraySize];
+    HAL_I2C_Master_Receive(&i2c_,ACC_ADDRESS, bytes_received,I2CReadSize, HAL_MAX_DELAY);
+
+    // 16-bit values
+    raw_acceleration_data_[0] = (bytes_received[1] <<8 | bytes_received[0]);
+    raw_acceleration_data_[1] = (bytes_received[3] <<8 | bytes_received[2]);
+    raw_acceleration_data_[2] = (bytes_received[5] <<8 | bytes_received[4]);
+}
+
+}
+
+
+
+
+
