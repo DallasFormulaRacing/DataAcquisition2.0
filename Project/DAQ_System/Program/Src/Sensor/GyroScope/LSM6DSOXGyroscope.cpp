6e160975fc60d69c87a67b2f46f5556507a0dc8a
William Lim <william.lim781@gmail.com>
2024-02-05 10:59:09 -0600

    created a library based for the gyroscope on the LSM6DSOX that is based on the l3gd20h library

---
+++ Project/DAQ_System/Program/Src/Sensor/GyroScope/LSM6DSOXGyroscope.cpp
@@ -0,0 +1,107 @@
+/*
+* Gyroscope L3GD20H
+* Authors:       William Lim
+*
+* Email:         wll180000@utdallas.edu
+*
+* (c) 2023 Dallas Formula Racing - Embedded Firmware Team
+* Formula SAE International Collegiate Chapter
+* GPL-3.0 License
+*/
+
+#include "LSM6DSOXGyroscope.hpp"
+#include <stdint.h>
+
+
+namespace sensor{
+
+LSM6DSOX::LSM6DSOX(I2C_HandleTypeDef& hi2c):hi2c_(hi2c){
+    WriteReg(GYR_ADDRESS, LSM6DSOX_CTRL2_G, CTRL2_G_init_value);
+    ComputeInitialOffset();
+}
+
+bool LSM6DSOX::ReadRawData(short arr[3]) {
+    uint8_t bytes[6] = {0};
+
+    if (Receive(bytes)) {
+        arr[0] = (bytes[1] << 8 | bytes[0]);
+        arr[1] = (bytes[3] << 8 | bytes[2]);
+        arr[2] = (bytes[5] << 8 | bytes[4]);
+
+        return true;
+    }
+
+    return false;
+}
+
+bool LSM6DSOX::AngularVelocity() {
+    short raw_data[3] = {0};
+    if (ReadRawData(raw_data)) {
+        angular_velocity_[0] =  raw_data[0] - offset_average_[0];
+        angular_velocity_[1] =  raw_data[1] - offset_average_[1];
+        angular_velocity_[2] =  raw_data[2] - offset_average_[2];
+
+        return true;
+    }
+
+    return false;
+}
+
+short* LSM6DSOX::DegreesPerSecond() {
+    // Equation: degrees/second = RawData * SensitivityOfFullScaleRange
+    // Using a default sensitivity for L3GD20H is 250dps
+	AngularVelocity();
+    degrees_per_second_[0] = angular_velocity_[0] * DPS_250_Sensitivity;
+    degrees_per_second_[1] = angular_velocity_[1] * DPS_250_Sensitivity;
+    degrees_per_second_[2] = angular_velocity_[2] * DPS_250_Sensitivity;
+
+    return degrees_per_second_;
+}
+
+float* LSM6DSOX::RadiansPerSecond() {
+	AngularVelocity();
+    radians_per_second_[0] = angular_velocity_[0] * DPS_250_Sensitivity * DegreesToRadians;
+    radians_per_second_[1] = angular_velocity_[1] * DPS_250_Sensitivity * DegreesToRadians;
+    radians_per_second_[2] = angular_velocity_[2] * DPS_250_Sensitivity * DegreesToRadians;
+
+    return radians_per_second_;
+}
+
+void LSM6DSOX::ComputeInitialOffset(){
+    short temp_data[3] = {0};
+    short sum[3] = {0};
+    uint8_t average_sample_size = 15;
+
+    for (int i = 0; i <= average_sample_size; i++) {
+        ReadRawData(temp_data);
+        sum[0] += temp_data[0];
+        sum[1] += temp_data[1];
+        sum[2] += temp_data[2];
+    }
+
+    offset_average_[0] = sum[0] / average_sample_size;
+    offset_average_[1] = sum[1] / average_sample_size;
+    offset_average_[2] = sum[2] / average_sample_size;
+}
+
+
+bool LSM6DSOX::WriteReg(uint8_t addr_i2c, uint8_t addr_reg, uint8_t register_data){
+    uint8_t data[2] = {addr_reg, register_data};
+    return HAL_I2C_Master_Transmit(&hi2c1,addr_i2c, data,2, HAL_MAX_DELAY);
+}
+
+
+
+bool LSM6DSOX::Receive(uint8_t data_buf[]){
+	uint8_t addr_reg = L3GD20_OUT_X_L | 0x80; // to set sequential reading you need to make the MSB of the register address to 1;
+	if(HAL_I2C_Master_Transmit(&hi2c1, GYR_ADDRESS, &addr_reg, 1, HAL_MAX_DELAY) != HAL_OK){
+		return false;
+	}
+	if(HAL_I2C_Master_Receive(&hi2c1, GYR_ADDRESS, data_buf, 6, HAL_MAX_DELAY) != HAL_OK){
+		return false;
+	}
+
+	return true;
+}
+
+}
