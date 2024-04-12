6ec0ff728003376bb834fda617df41325b3e934a
NicoOhR <nimoohayonr@gmail.com>
2024-04-12 15:27:16 -0500

    changed names to match ;P

--- Project/DAQ_System/Program/Src/Platform/I2C/STM/I2CStmF4.cpp
+++ Project/DAQ_System/Program/Src/Platform/I2C/STM/I2CStmF4.cpp
@@ -9,18 +9,18 @@
 namespace platform {
 
 	I2CStmF4::I2CStmF4(I2C_HandleTypeDef &hi2c)
-			: _i2c_(hi2c) {
+			: i2c_(hi2c) {
 	}
 
 	I2CStmF4::~I2CStmF4(){}
 
 	void I2CStmF4::Receive(uint8_t *response, uint8_t dev_add_, uint8_t response_size_){
-		HAL_I2C_Master_Receive(&_i2c_, dev_add_, response, response_size_, HAL_MAX_DELAY);
+		HAL_I2C_Master_Receive(&i2c_, dev_add_, response, response_size_, HAL_MAX_DELAY);
 	}
 
 	void I2CStmF4::Transmit(uint8_t *commands, uint8_t dev_add_, uint8_t maxBytes){
-				HAL_I2C_Master_Transmit(&_i2c_,dev_add_, commands, maxBytes, HAL_MAX_DELAY);
-			}
+		HAL_I2C_Master_Transmit(&i2c_,dev_add_, commands, maxBytes, HAL_MAX_DELAY);
+	}
 
 }
 
